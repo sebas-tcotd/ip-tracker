df043f8844f103f556857afc609f1662bb87f065
Sebastian Vargas <vargaspizango1@gmail.com>
2024-04-10 13:13:20 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Ip Address Tracker</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/ip-tracker/IpTracker.tsx
@@ -0,0 +1,3 @@
+export const IpTracker = () => {
+  return <div>IpTracker</div>;
+};
